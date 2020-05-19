e5ae00990e3ba01deca55317b3c8c18a3b524a72
Gianmarco Folchi <gmfolchi@hotmail.com>
2020-05-19 17:34:25 -0400

    Initial Commit

---
+++ EmojiDictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiDictionary
+//
+//  Created by HS Lab 21 on 5/19/20.
+//  Copyright © 2020 HS Lab 21. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
